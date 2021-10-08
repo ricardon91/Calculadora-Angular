081da838274ac29dfc70b0802579abd68d5d551c
ricardon91 <ricardo_l.h.p@hotmail.com>
2021-10-07 22:35:00 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora';
+  title = 'Calculadora Prova Dev Web';
+
+  value1: number | undefined;
+  value2: number | undefined;
+  resultado: number | undefined;
+  operador: string | undefined  
+
+  constructor() { }
+
+  getOperaracao(op:string){
+    debugger
+    this.operador = op;
+  }
+
+  calcular(){
+    debugger
+    this.operador;
+    if(this.operador == '+'){
+      this.soma();
+    }else if(this.operador == '-'){
+      this.subtracao();
+    }else if(this.operador == '*'){
+      this.multiplicacao();
+    }else if(this.operador == '/'){
+      this.divisao();
+    }
+  }
+  
+
+  soma() {
+    this.resultado = Number(this.value1) + Number(this.value2);
+  }
+
+  subtracao() {
+    this.resultado = Number(this.value1) - Number(this.value2);
+  }
+
+  multiplicacao() {
+    this.resultado = Number(this.value1) * Number(this.value2);
+  }
+
+  divisao() {
+    if (Number(this.value2) != 0) {
+      this.resultado = Number(this.value1) / Number(this.value2);
+    }
+    else {
+      alert('Não é possivel efetuar divisão por 0');
+    }
+  }
+
 }
